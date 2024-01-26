b237db4c5ac175b79af3d2dc577ced0ed252b416
Milan1302 <rastogimilan2003@gmail.com>
2024-01-26 12:47:12 +0530

    fixes

--- src/components/Infocard.js
+++ src/components/Infocard.js
@@ -6,7 +6,7 @@ export default function Infocard(props) {
                 <div class="card-body">
                     <h2 class="card-title gradient-text">{props.title}</h2>
                     <p class="card-text">{props.text}</p>
-                    <a href={props.link} class="btn btn-dark" target='blank'>Explore</a>
+                    <a href={props.link} class="btn btn-dark" target={props.target}>Explore</a>
                 </div>
             </div>
     )
